433bbded25b34de308bbb2c1e8e7da477eb03d73
Clint Ayres <cayres@cayres-mac.local>
2016-10-14 23:29:15 -0400

    init :rocket:

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# redux-offline-store-enhancer
-Automatically detect if you are online or offline.
+# redux-online-store-enhancer
+Automatically detect if you are online or online.
+
+### Install
+
+```js
+npm i -S redux-online-store-enhancer
+```
+
+### Example
+
+```js
+import onlineStoreEnhancer from 'redux-online-store-enhancer';
+
+// also can be added to applyMiddleware() in more advanced configurations
+const store = createStore(reducer, onlineStoreEnhancer);
+
+// get from state directly
+const { online } = store.getState();
+
+// reselect
+const selectOffline = (state) => state.online;
+```
+
+Global app state now has a top level, _online_, attribute that will dynamically change as browser goes on and online.
---
+++ onlineStoreEnhancer.js
@@ -0,0 +1,38 @@
+
+function onlineReducerEnhancer(rootReducer) {
+  return (state, action) => ({
+    ...rootReducer(state, action),
+    online: reduceOnline(state.online, action),
+  });
+}
+
+function reduceOnline(online = false, action) {
+  if (action.type === '@@ONLINE') {
+    return action.online;
+  }
+  return online;
+}
+
+function updateOnlineStatus(dispatch, getState) {
+  return (event) => {
+    const condition = navigator.onLine;
+    const { online } = getState();
+    if (condition !== online) {
+      dispatch({ type: '@@ONLINE', online: condition });
+    }
+  };
+}
+
+export default function onlineStoreEnhancer() {
+  return (createStore) => (reducer, preloadedState) => {
+    const enhancedReducer = onlineReducerEnhancer(reducer);
+    const store = createStore(enhancedReducer, preloadedState);
+    const {dispatch, getState} = store;
+    if (window) {
+      window.addEventListener('online',  updateOnlineStatus(dispatch, getState));
+      window.addEventListener('online', updateOnlineStatus(dispatch, getState));
+    } else {
+      console.error('No window object, maintainer needs to add support for React Native, PR welcome');
+    }
+  }
+}
